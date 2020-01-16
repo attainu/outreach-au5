a9f8c6364a2beea182a9d875158e36426d961358
shubhankar-bisht-au5 <56347792+shubhankar-bisht-au5@users.noreply.github.com>
2020-01-16 19:08:43 +0530

    Main app file

---
+++ app.js
@@ -0,0 +1,56 @@
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose"),
+    passport    = require("passport"),
+    cookieParser = require("cookie-parser"),
+    LocalStrategy = require("passport-local"),
+    flash        = require("connect-flash"),
+    Event  = require("./models/event"),
+    Comment     = require("./models/comment"),
+    User        = require("./models/user"),
+    session = require("express-session"),
+    seedDB      = require("./seeds"),
+    methodOverride = require("method-override");
+    
+//requiring routes
+var commentRoutes    = require("./routes/comments"),
+    eventRoutes = require("./routes/events.js"),
+    indexRoutes      = require("./routes/index")
+    
+mongoose.connect("mongodb://localhost/outreach");
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride('_method'));
+app.use(cookieParser('secret'));
+
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "5855adwqqd8q88q88qqe",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+   res.locals.currentUser = req.user;
+   res.locals.success = req.flash('success');
+   res.locals.error = req.flash('error');
+   next();
+});
+
+
+app.use("/", indexRoutes);
+app.use("/event", eventRoutes);
+app.use("/events/:id/comments", commentRoutes);
+
+app.listen(3000, function(){
+   console.log("The Server Has Started!");
+});
